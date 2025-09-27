d1cbddaed043e0cc2387f2ccade175631fdb5924
aldiyar_aidarkhan <aldiyar.new@gmail.com>
2025-09-27 16:42:29 +0500

    first commit

---
+++ src/main/java/io/github/ragecoo/dusk_music/DuskMusicApplication.java
@@ -0,0 +1,13 @@
+package io.github.ragecoo.dusk_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuskMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuskMusicApplication.class, args);
+	}
+
+}
